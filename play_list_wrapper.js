776960613359f1b9c69a6e114bd07cf831666c78
hjkwon <widruv@gmail.com>
2018-07-31 22:26:27 +0900

    lyrics working
    todo
    싱크 구조 설계
    가사 넣기
    media query

---
+++ play_list_wrapper.js
@@ -0,0 +1,68 @@
+var PlayListWrapper = {
+  show:function(id) {
+    var ul = document.getElementById('play_list');
+    for (var i in PlayLists.lists) {
+      var list = document.createElement("li");
+      list.classList.add('list_item');
+      var check = document.createElement("input");
+      check.type = "checkbox";
+      check.classList.add("select_check");
+      var a = document.createElement("a");
+      a.href = "#";
+      $(a).attr('music_id', i);
+      a.onclick = function() {
+        var audio = $('#audio');
+        var index = $(this).attr('music_id');
+        $('#mp3_src').attr('src', 'music/'+index+'.mp3');
+        audio[0].pause();
+        audio[0].load();
+        audio[0].oncanplaythrough = audio[0].play();
+        $('#cover_image').attr('src', 'music/Folder.jpg?timestamp='+Math.random());
+        $('.player').css('background-image', 'url(music/Folder.jpg)');
+
+        // for (var i in Lyrics.lyric[index].lines)
+        // {
+        //   console.log(Lyrics.lyric[index].lines[i]);
+        // }
+
+      }
+      a.innerHTML = '<span class="title_ellipsis" sort-field="SONG">'
+                  + PlayLists.lists[i].title + '</span><br>';
+      a.innerHTML += '<span class="artist_ellipsis" sort-field="ARTIST">'
+                  + PlayLists.lists[i].singer + '</span>';
+      var btn_listen = document.createElement('a');
+      btn_listen.classList.add('btn_listen');
+      btn_listen.href = '#';
+      $(btn_listen).attr('music_id', i);
+      btn_listen.onclick = function() {
+        var audio = $('#audio');
+
+        $('#mp3_src').attr('src', 'music/'+$(this).attr('music_id')+'.mp3');
+        audio[0].pause();
+        audio[0].load();
+        audio[0].oncanplaythrough = audio[0].play();
+        $('#cover_image').attr('src', 'music/Folder.jpg?timestamp='+Math.random());
+        $('.player').css('background-image', 'url(music/Folder.jpg)');
+      }
+      btn_listen.innerHTML = '<img src="play.png" width="30px" heigth="30px">';
+      var toggle_btn_box = document.createElement('div');
+      toggle_btn_box.classList.add('toggle_btn_box');
+      toggle_btn_box.innerHTML = '<a href="#"><i class="material-icons">menu</i></a>';
+      list.appendChild(check);
+      list.appendChild(a);
+      list.appendChild(btn_listen);
+      list.appendChild(toggle_btn_box);
+      list.classList.add('list_item');
+      list.onmouseover = function() {
+        this.style.backgroundColor = "rgb(233,240,244)";
+        this.children[2].style.opacity = "0.3";
+      }
+      list.onmouseout = function() {
+        $(this).css('backgroundColor', 'white');
+        this.children[2].style.opacity = "0";
+      }
+      $(ul).append(list);
+    }
+    $(id).append(ul);
+  }
+}
---
+++ play_lists.js
@@ -0,0 +1,33 @@
+var PlayLists = {
+  lists: [
+    {"title":"RPG", "singer":"sekai no owari"},
+    {"title":"残酷な天使のテーゼ", "singer":"高橋 洋子"},
+    {"title":"Rain", "singer":"sekai no owari"},
+    {"title":"Dragon Night", "singer":"sekai no owari"},
+    {"title":"幻の命", "singer":"sekai no owari"},
+    {"title":"Happy End", "singer":"Back Number"},
+    {"title":"himawari", "singer":"Mr.Children"},
+    {"title":"hanabi", "singer":"Mr.Children"},
+    {"title":"tears", "singer":"X-Japan"},
+    {"title":"Soup", "singer":"藤原さくら"},
+    {"title":"好きよ好きよ好きよ", "singer":"藤原さくら"},
+    {"title":"PIKANCHI", "singer":"嵐"},
+    {"title":"WISH", "singer":"嵐"},
+    {"title":"きっと大丈夫", "singer":"嵐"},
+    {"title":"A.RA.SHI", "singer":"嵐"},
+    {"title":"Happiness", "singer":"嵐"},
+    {"title":"Beautiful days", "singer":"嵐"},
+    {"title":"Face Down", "singer":"嵐"},
+    {"title":"Feel Your Breeze", "singer":"V6"},
+    {"title":"恋", "singer":"星野源"}
+  ]
+}
+
+var Lyrics = {
+  lyric : [
+    {"title": "RPG", "lines": [
+      "空は青く澄み渡り",
+      "海を目指して歩く"
+    ]}
+  ]
+}
